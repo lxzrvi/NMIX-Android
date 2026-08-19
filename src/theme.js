36411430f42c9db62dd739895a46b97d66f72880
lxzrvi <lxzrvi@gmail.com>
2026-08-19 08:01:49 +0530

    Create theme.js

---
+++ src/theme.js
@@ -0,0 +1,74 @@
+export const themes = {
+  green: {
+    accent: '#319b79',
+    accentDark: '#216e56',
+    accentLight: '#69d6b2',
+    topOne: '#19493a',
+    topTwo: '#319b79',
+    topThree: '#173e33'
+  },
+
+  blue: {
+    accent: '#348bb8',
+    accentDark: '#225e7d',
+    accentLight: '#75c8ef',
+    topOne: '#143a50',
+    topTwo: '#348bb8',
+    topThree: '#102c3e'
+  },
+
+  purple: {
+    accent: '#8a62c8',
+    accentDark: '#60428f',
+    accentLight: '#c2a1ef',
+    topOne: '#33224d',
+    topTwo: '#865ac0',
+    topThree: '#241b37'
+  },
+
+  orange: {
+    accent: '#d57d35',
+    accentDark: '#92531f',
+    accentLight: '#efad73',
+    topOne: '#563116',
+    topTwo: '#c8712d',
+    topThree: '#392313'
+  },
+
+  rose: {
+    accent: '#c85878',
+    accentDark: '#893950',
+    accentLight: '#ef91ad',
+    topOne: '#542338',
+    topTwo: '#c85878',
+    topThree: '#351722'
+  }
+};
+
+export const lightColors = {
+  body: '#dedede',
+  surface: '#eeeeee',
+  surface2: '#dedede',
+  surface3: '#d1d8d5',
+  text: '#202321',
+  muted: '#66706c',
+  border: '#bec5c2'
+};
+
+export const darkColors = {
+  body: '#0d1110',
+  surface: '#171c1a',
+  surface2: '#202624',
+  surface3: '#2a322f',
+  text: '#edf4f1',
+  muted: '#a4afaa',
+  border: '#303936'
+};
+
+export const fontChoices = [
+  'Poppins',
+  'Inter',
+  'Outfit',
+  'Nunito',
+  'Quicksand'
+];
