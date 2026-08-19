6aee15de90d094820c394aaa058fa434b24ab0b7
lxzrvi <lxzrvi@gmail.com>
2026-08-19 20:55:03 +0530

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -1,30 +1,44 @@
-import React, { useEffect } from 'react';
+import React, {
+  useEffect
+} from 'react';
 
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import * as SplashScreen from 'expo-splash-screen';
+import {
+  Stack
+} from 'expo-router';
 
-SplashScreen.preventAutoHideAsync().catch(() => {});
+import {
+  StatusBar
+} from 'expo-status-bar';
+
+import * as SplashScreen
+  from 'expo-splash-screen';
+
+SplashScreen
+  .preventAutoHideAsync()
+  .catch(() => {});
 
 export default function RootLayout() {
   useEffect(() => {
-    const id = setTimeout(() => {
-      SplashScreen.hideAsync().catch(() => {});
-    }, 150);
-
-    return () => clearTimeout(id);
+    SplashScreen
+      .hideAsync()
+      .catch(() => {});
   }, []);
 
   return (
     <>
-      <StatusBar style="light" />
+      <StatusBar
+        style="light"
+      />
 
       <Stack
         screenOptions={{
           headerShown: false,
+
           animation: 'fade',
+
           contentStyle: {
-            backgroundColor: '#07110f'
+            backgroundColor:
+              '#07110f'
           }
         }}
       />
